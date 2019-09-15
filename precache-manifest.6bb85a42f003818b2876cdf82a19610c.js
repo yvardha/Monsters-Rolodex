af326805f0a3c0a9b7d804c78e4922407515313f
Yash Vardhan <yashvardhanu@example.com>
2019-09-15 13:48:07 +0530

    Updates

---
+++ precache-manifest.6bb85a42f003818b2876cdf82a19610c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de203cac24b689bfe1d3dd806e68994",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "f29c6e2783d83893220a",
+    "url": "/Monsters-Rolodex/static/css/main.eef2cdf0.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/Monsters-Rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "f29c6e2783d83893220a",
+    "url": "/Monsters-Rolodex/static/js/main.8f118f6a.chunk.js"
+  },
+  {
+    "revision": "8a7f45627e3585acf36b",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.3bf5044b.js"
+  }
+]);
